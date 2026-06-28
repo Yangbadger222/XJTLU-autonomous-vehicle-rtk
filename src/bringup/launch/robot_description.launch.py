a65e6ed83e501583fe16f8deb96cac8eab8db756
yoon-zh <yoon_zh@outlook.com>
2026-06-28 20:08:05 +0800

    fix: added xacro findexecutable for robot_description launch file

--- src/bringup/launch/robot_description.launch.py
+++ src/bringup/launch/robot_description.launch.py
@@ -1,7 +1,7 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.substitutions import Command
+from launch.substitutions import Command, FindExecutable
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 
@@ -14,10 +14,12 @@ def generate_launch_description():
         'rosbot.urdf.xacro'
     )
 
-    # Use Command to evaluate the xacro file into a standard URDF string
-    robot_desc = ParameterValue(Command(['xacro ', xacro_path]), value_type=str)
+    # Use FindExecutable to guarantee the system finds the xacro binary
+    robot_desc = ParameterValue(
+        Command([FindExecutable(name='xacro'), ' ', xacro_path]), 
+        value_type=str
+    )
 
-    # Pass the compiled URDF to the Robot State Publisher
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',

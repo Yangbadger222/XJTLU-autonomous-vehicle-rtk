33be968897ba28122dde1e3cdf39ff251d1d83b0
yoon-zh <yoon_zh@outlook.com>
2026-06-29 16:38:20 +0800

    chore: added URDF launch in system_nav_gps launch

--- src/bringup/launch/system_nav_gps.launch.py
+++ src/bringup/launch/system_nav_gps.launch.py
@@ -133,6 +133,12 @@ def generate_launch_description():
     delayed_nav2 = TimerAction(period=5.0, actions=[nav2_launch])
     delayed_goal_manager = TimerAction(period=7.0, actions=[goal_manager_node])
 
+    urdf_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(bringup_share, 'launch', 'robot_description.launch.py')
+        )
+    )
+
     return LaunchDescription(
         [
             DeclareLaunchArgument(
@@ -180,5 +186,6 @@ def generate_launch_description():
             route_lifecycle_manager,
             delayed_nav2,
             delayed_goal_manager,
+            urdf_launch,
         ]
     )

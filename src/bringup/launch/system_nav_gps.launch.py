5140b1ba7a52556b9430af619a6980f2d960b01c
yoon-zh <yoon_zh@outlook.com>
2026-07-01 18:49:17 +0800

    test: re-enabled urdf launch in system_nav_gps.launch.py

--- src/bringup/launch/system_nav_gps.launch.py
+++ src/bringup/launch/system_nav_gps.launch.py
@@ -134,11 +134,11 @@ def generate_launch_description():
     delayed_goal_manager = TimerAction(period=7.0, actions=[goal_manager_node])
 
     # Does not currently work as intended. Review for later
-    # urdf_launch = IncludeLaunchDescription(
-    #     PythonLaunchDescriptionSource(
-    #         os.path.join(bringup_share, 'launch', 'robot_description.launch.py')
-    #     )
-    # )
+    urdf_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(bringup_share, 'launch', 'robot_description.launch.py')
+        )
+    )
 
     return LaunchDescription(
         [
@@ -187,6 +187,6 @@ def generate_launch_description():
             route_lifecycle_manager,
             delayed_nav2,
             delayed_goal_manager,
-            # urdf_launch,
+            urdf_launch,
         ]
     )
